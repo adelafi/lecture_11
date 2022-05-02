aa1478299752517da33c35ff05f870df9e5bdd37
Adela Fialova <adelafialova21@gmail.com>
2022-05-02 10:22:58 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    number = recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+    return number
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(7))
 
 
 if __name__ == "__main__":
